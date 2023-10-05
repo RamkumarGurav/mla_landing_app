8664175119b5ac0507369edca762942c4c24b245
ramkumar gurav <ramkumar.1js15ec071@gmail.com>
2023-10-05 23:13:51 +0530

    added notfound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route,Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return <>
     <Routes>
       <Route path="/" element={<HomePage/>}></Route>
-      <Route path="*" element={<HomePage/>}></Route>
+      <Route path="*" element={<NotFound/>}></Route>
     </Routes>
   </>;
 };
